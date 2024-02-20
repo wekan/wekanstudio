7d65feccfc5d5925a1c68b9ef7e84648ca20681f
Lauri Ojansivu <x@xet7.org>
2024-02-20 05:40:39 +0200

    Added translations files.

---
+++ releases/translations/pull-translations.sh
@@ -0,0 +1,10 @@
+#cd ~/repos/wekan
+
+tx --config .tx/config pull -a -f
+
+# https://developers.transifex.com/docs/cli
+# New Go-based transifex client.
+# 1. Migrated: tx migrate
+# 2. Pushed all: tx push -a -l
+# 3. Pulled all: tx pull -a -l
+# Although now all show as translated, maybe I did something wrong.
