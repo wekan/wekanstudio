9d1ce19cae60fbb2b09096c4841853a203b734c2
Lauri Ojansivu <x@xet7.org>
2024-08-21 03:41:46 +0300

    Updated to redbean 3.0. Updated assets.

--- build.sh
+++ build.sh
@@ -1,15 +1,24 @@
 
-# Make redbean executeable file type working
-sudo sh -c "echo ':APE:M::MZqFpD::/usr/bin/ape:' >/proc/sys/fs/binfmt_misc/register"
-sudo sh -c "echo ':APE-jart:M::jartsr::/usr/bin/ape:' >/proc/sys/fs/binfmt_misc/register"
+# Linux: Make redbean executeable file type working
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  sudo sh -c "echo ':APE:M::MZqFpD::/usr/bin/ape:' >/proc/sys/fs/binfmt_misc/register"
+  sudo sh -c "echo ':APE-jart:M::jartsr::/usr/bin/ape:' >/proc/sys/fs/binfmt_misc/register"
+fi
+
+# https://redbean.dev
+# redbean v3.0.0 was released on 2024-08-17.
+# redbean-3.0.0.com
+# 5.5m - PE+ELF+MachO+ZIP+SH for AMD64 and ARM64 (source code)
+# 382f1288bb96ace4bab5145e7df236846c33cc4f1be69233710682a9e71e7467
 
 # Copy redbean.com.template to wekan.com
-cp -f redbean.com.template wekan.com
+cp -f redbean-3.0.0.com.template wekan.com
 
 # Add all files from srv directory to zip file at end of wekan.com
 cd srv/
 zip -r ../wekan.com `ls -A`
 cd ..
+chmod +x wekan.com
 
 # Start WeKan
 ./wekan.com
